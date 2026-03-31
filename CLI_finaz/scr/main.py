c2e41453bfeaabaae9c039cc8c206b3aa0ce3e96
jonadss <jonas.hiekel@gmail.com>
2026-03-31 21:05:23 +0200

    cli test

--- CLI_finaz/scr/main.py
+++ CLI_finaz/scr/main.py
@@ -1,15 +1,54 @@
 from rich.console import Console
 from rich.panel import Panel
+from rich.align import Align
+from rich import box
+from rich.live import Live
+from rich.console import Group
+from rich.styled import Styled
+from rich.table import Table
+
+# Instead of f"[red]{main_panel_content}[/red]"
+
+
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 
 console = Console()
 
+
+
+
+
+
 def start_app():
-    console.print(Panel.fit(
-        "[#ff4d4d]Mein CLI Interface[/#ff4d4d]", 
+
+    rat = r"""
+            ____()()        
+          /      @@     
+    `~~~~~\_;m__m._>o           
+    """ 
+    app_title = """[bold red]
+    
+    Finaz-App
+
+    [/bold red]"""
+
+    layout_table = Table.grid(expand=True) # expand=True pushes the columns to the edges
+    layout_table.add_column(justify="left")
+    layout_table.add_column(justify="right")
+    layout_table.add_row(Align.center(app_title), rat)
+
+    
+
+    console.print(Panel(
+        layout_table,
+        border_style="cyan",
         title= "MCI-App",
-        subtitle="v1.0"))
+        subtitle="v1.0",
+        width = 90
+
+
+        ))
     
     # Hier definieren wir die Wörter für die Vervollständigung
     befehle = ["addieren", "hilfe", "beenden", "status", "einstellungen"]
