e7857194ca38370d1a0b16f68950d87bfb57b042
jonadss <jonas.hiekel@gmail.com>
2026-04-06 23:21:40 +0200

    we

--- CLI_finaz/scr/main.py
+++ CLI_finaz/scr/main.py
@@ -7,7 +7,7 @@ from ui_styles import UI
 
 def start_app():
     start_ui = UI()
-    start_ui.draw_header("start")
+    start_ui.draw_welcom_panel(2)
 
     command = ["start", "help", "exit"]
     command_completer = WordCompleter(command, ignore_case=True)
@@ -26,9 +26,9 @@ def start_app():
                 print("no help for you")
 
             elif user_input == "start":
-                start_ui.draw_header("you will be forwarded")
+                start_ui.play_loading("Load bank balance ...")
                 start_balance_overview()
-                break
+                
         
         
         
@@ -48,6 +48,288 @@ def start_app():
         # Fängt Strg+D ab
         except EOFError: 
             break
+
+
+def start_balance_overview():
+    
+
+
+    start_ui = UI()
+    start_ui.draw_header("start_balance_overview")  
+    
+    command = ["budget","categories","debts","importer", "help", "exit", "back"]
+    command_completer = WordCompleter(command, ignore_case=True)
+
+    while True:
+        try:
+            user_input = prompt(
+                "balance_overview -- enter command: ", 
+                completer=command_completer
+            ).strip().lower()
+
+            if user_input == "exit":
+                sys.exit(1)
+            elif user_input == "back":
+                return
+                
+
+            elif user_input == "help":
+                print("no help for you")
+
+
+
+
+
+            elif user_input == "categories":
+                from app_categories import start_categories
+                start_ui.draw_header("you will be forwarded")
+                start_categories()
+
+            elif user_input == "debts":
+                from app_debts import start_debts
+                start_ui.draw_header("you will be forwarded")
+                start_debts()
+            
+            elif user_input == "importer":
+                from app_importer import start_importer
+                start_ui.draw_header("you will be forwarded")
+                start_importer()
+            
+            elif user_input == "budget":
+                from app_budget import start_budget
+                start_ui.draw_header("you will be forwarded")
+                start_budget()
+
+
+
+
+            elif user_input == "":
+                continue
+            else: 
+                console.print(f"[red]Unknown command:[/red] {user_input}")
+
+        # Fängt Strg+C ab
+        except KeyboardInterrupt: 
+            return sys.exit(0)
+        # Fängt Strg+D ab
+        except EOFError: 
+            return sys.exit(0)
+
+##########################################################
+#####habs nichts gemacht daran#########################
+###########################################################################
+
+
+def start_budget():
+    
+    start_ui = UI()
+    start_ui.draw_header("start_budget")
+    
+    command = ["start", "help", "exit", "back"]
+    command_completer = WordCompleter(command, ignore_case=True)
+
+    while True:
+        try:
+            user_input = prompt(
+                "categories -- enter command: ", 
+                completer=command_completer
+            ).strip().lower()
+
+            if user_input == "exit":
+                break
+            elif user_input == "back":
+                return
+
+            elif user_input == "help":
+                print("no help for you")
+
+            elif user_input == "start":
+                start_ui.draw_header("you will be forwarded")
+                
+    
+
+
+
+
+
+
+
+
+
+            elif user_input == "":
+                continue
+            else: 
+                console.print(f"[red]Unknown command:[/red] {user_input}")
+
+        # Fängt Strg+C ab
+        except KeyboardInterrupt: 
+            break
+        # Fängt Strg+D ab
+        except EOFError: 
+            break
+
+
+
+
+def start_categories():
+    
+    start_ui = UI()
+    start_ui.draw_header("start_categories")
+    
+    command = ["start", "help", "exit", "back"]
+    command_completer = WordCompleter(command, ignore_case=True)
+
+    while True:
+        try:
+            user_input = prompt(
+                "categories -- enter command: ", 
+                completer=command_completer
+            ).strip().lower()
+
+            if user_input == "exit":
+                break
+            elif user_input == "back":
+                return
+
+            elif user_input == "help":
+                print("no help for you")
+
+            elif user_input == "start":
+                start_ui.draw_header("you will be forwarded")
+                
+    
+
+
+
+
+
+
+
+
+
+            elif user_input == "":
+                continue
+            else: 
+                console.print(f"[red]Unknown command:[/red] {user_input}")
+
+        # Fängt Strg+C ab
+        except KeyboardInterrupt: 
+            break
+        # Fängt Strg+D ab
+        except EOFError: 
+            break
+
+
+
+
+
+
+
+
+
+
+
+def start_debts():
+    
+    start_ui = UI()
+    start_ui.draw_header("start_debts")
+    
+    command = ["start", "help", "exit", "back"]
+    command_completer = WordCompleter(command, ignore_case=True)
+
+    while True:
+        try:
+            user_input = prompt(
+                "debts -- enter command: ", 
+                completer=command_completer
+            ).strip().lower()
+
+            if user_input == "exit":
+                break
+            elif user_input == "back":
+                return
+
+            elif user_input == "help":
+                print("no help for you")
+
+            elif user_input == "start":
+                start_ui.draw_header("you will be forwarded")
+                
+    
+
+
+
+
+
+
+
+
+
+            elif user_input == "":
+                continue
+            else: 
+                console.print(f"[red]Unknown command:[/red] {user_input}")
+
+        # Fängt Strg+C ab
+        except KeyboardInterrupt: 
+            break
+        # Fängt Strg+D ab
+        except EOFError: 
+            break
+
+
+
+
+
+
+def start_importer():
+    
+    start_ui = UI()
+    start_ui.draw_header("start_importer")
+    
+    command = ["start", "help", "exit", "back"]
+    command_completer = WordCompleter(command, ignore_case=True)
+
+    while True:
+        try:
+            user_input = prompt(
+                "importer -- enter command: ", 
+                completer=command_completer
+            ).strip().lower()
+
+            if user_input == "exit":
+                break
+            elif user_input == "back":
+                return
+
+            elif user_input == "help":
+                print("no help for you")
+
+            elif user_input == "start":
+                start_ui.draw_header("you will be forwarded")
+                
+
+
+
+
+
+
+
+
+
+            elif user_input == "":
+                continue
+            else: 
+                console.print(f"[red]Unknown command:[/red] {user_input}")
+
+        # Fängt Strg+C ab
+        except KeyboardInterrupt: 
+            break
+        # Fängt Strg+D ab
+        except EOFError: 
+            break
+
+
 
 
 
--- CLI_finaz/scr/ui_styles.py
+++ CLI_finaz/scr/ui_styles.py
@@ -1,4 +1,8 @@
 from ui_toolkit import *
+import datetime
+import time
+import random
+
 
 class UI:
     def __init__(self):
@@ -11,43 +15,147 @@ class UI:
 
 
 
-frog_text = """
-  @..@
+
+    def draw_welcom_panel(self,duration = 5):
+
+        with Live(self._build_grid_panel(), console=self.console, refresh_per_second=10) as live:
+            start_time = time.time()
+            
+
+            while time.time() - start_time < duration:
+
+                is_winking = int(time.time() * 2) % 2 == 0
+
+                live.update(self._build_grid_panel(wink=is_winking))
+                time.sleep(0.1)
+
+            live.update(self._build_grid_panel(wink=False))
+
+
+
+    def play_loading(self,text="Lade Daten...",lenght=30):
+
+        frames = self.get_cachy_frames(lenght)
+        try:
+
+            self.console.show_cursor(False)
+
+            for f in frames:
+                
+                ani_text = Text(f)
+                ani_text.highlight_regex(r"C","bold green")
+                ani_text.highlight_regex(r"c","bold green")
+                ani_text.highlight_regex(r"•","white")
+                self.console.print(text,ani_text,, end="\r")
+                
+                time.sleep(0.1) 
+
+            self.console.print(" " * (lenght+1), end="\r") 
+
+        finally:
+            self.console.show_cursor(True)
+
+
+    def draw_exit_panel(self):
+
+        with Live(self._build_grid_panel(middle_title = "Good byeeee"), console=self.console, refresh_per_second=10) as live:
+          start_time = time.time()
+          
+        while time.time() - start_time < duration:
+            is_winking = int(time.time() * 2) % 2 == 0
+            live.update(self._build_grid_panel(wink=is_winking, middle_title = "Good byeeee"))
+            time.sleep(0.1)
+        live.update(self._build_grid_panel(wink=False, middle_title = "Good byeeee"))
+        
+
+
+
+
+###########################################################
+#######################Hilffunktion########################
+###########################################################
+
+
+
+    #get loiading animation UNCOLORIERT
+    def get_cachy_frames(self,width):
+        frame = []
+        for i in range(width + 1):
+            if i % 2 == 0:
+                cachy = "C"
+                eaten = "-" * i
+                remaining = " " * (width - i)
+            else:
+                cachy = "c"
+                eaten = "-" * i
+                remaining = "⚬" * (width - i)
+            
+            prozent_int = i/width * 100
+            prozent_char = "%"
+            frame.append(f"[{eaten}{cachy}{remaining}{prozent_int}{prozent_char}]")
+        return frame
+
+
+
+
+    #build grid for welcome panel 
+    def _build_grid_panel(self, wink=False, middle_title = "Welcome!!"):
+
+        current_date, current_time, app_status, app_title = self.get_context()
+        
+        frog_img = self.frog(wink=wink)
+
+        grid = Table.grid(expand=True)
+        grid.add_column(ratio=1)
+        grid.add_column(ratio=3)
+        grid.add_column(ratio=1)
+        text_middle_text = Text(middle_title,style= "#fa7ff6")
+       
+
+        grid.add_row(
+            frog_img,
+            Align.center(text_middle_text, vertical="middle"),
+            Align.right(
+                Group(app_status, "", current_date, current_time), 
+                vertical="top"
+            )
+        )
+        return Panel(grid, title=app_title, border_style="magenta" ,box=box.ROUNDED )
+
+
+
+    #give frog as text objekt with colour back
+    def frog(self, wink=False):
+        eyes = "-..-" if wink else "@..@"
+        
+        frog_text = f"""
+  {eyes}
  (----)
 ( >__< )
 ^^    ^^
 """
+        frog_green = Text(frog_text, style="bold green")
+        frog_green.highlight_regex(r"(@|-)",    "bold red")
+        frog_green.highlight_regex(r"-+",       "black")
+        frog_green.highlight_regex(r"[\(\)]",   "#248a3f")
+        frog_green.highlight_regex(r"\^",       "black")
+        return frog_green
 
-console = Console()  
-
-frog_green = Text(frog_text, style="bold green")
-frog_green.highlight_regex(r"@",        "bold red")
-frog_green.highlight_regex(r"-+",       "black")
-frog_green.highlight_regex(r"[\(\)]",   "#248a3f")
-frog_green.highlight_regex(r"\^",       "black")
-
-console.print(frog_green)
-
-title  = "[bold cyan]Finanz-App[/]"
-status = "[bold green]● Online[/]"
-
-
-
-grid = Table.grid(expand=True)
-grid.add_column(ratio=1)   # 25 % → Frosch
-grid.add_column(ratio=3)   # 75 % → Text
+    def get_context(self):
+        now = datetime.datetime.now()
+        date = now.strftime("%d.%m.%Y")
+        time_now = now.strftime("%H:%M")
+        status = "[bold green]● Online[/]"
+        title_text = "[bold green]Finanz-App[/]" 
+        
+        return date,time_now,status,title_text
 
 
 
-title  = "[bold cyan]Finanz-App[/]"
-status = "[bold green]● Online[/]"
-grid.add_row(
-    frog_green,
-    Align.center(
-        "[bold]Welcome![/]\n",
-        vertical="middle",
-    ),
-)
 
+###########################################################
+#####################Sytemfunktionen#######################
+###########################################################
 
-console.print(Panel(grid, title="[blue]finanz_frog[/]", border_style="magenta"))
+    def sytem_exit(self):
+        sys.exit(1)
